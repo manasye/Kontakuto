320b640acd2a92cf86969bd0496272b41155fc39
manasye <manase32@gmail.com>
2023-10-02 08:49:47 +0700

    chore: add contact

---
+++ src/types/AddContactResponse.ts
@@ -0,0 +1,5 @@
+import Contact from "./Contact";
+
+export default interface AddContactResponse {
+  insert_contact_one: Contact;
+}
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,25 +1,25 @@
-export const generateColorFromInitials = (initials: string) => {
-    const colors = [
-        '#FFC0CB',
-        '#FFA07A',
-        '#FFD700',
-        '#98FB98',
-        '#87CEEB',
-        '#FFB6C1',
-        '#FFDAB9',
-        '#FFA500',
-        '#FF69B4',
-        '#B0E0E6',
-        '#98FB98',
-        '#FF6347',
-        '#20B2AA',
-        '#9370DB',
-        '#FF4500',
-        '#F0E68C',
-        '#F08080',
-        '#FFA07A'
-    ];
-    const charSum = initials.charCodeAt(0) + initials.charCodeAt(1);
-    const colorIndex = charSum % colors.length;
-    return colors[colorIndex];
+export const generateColorFromInitials = (initials: string = "") => {
+  const colors = [
+    "#FFC0CB",
+    "#FFA07A",
+    "#FFD700",
+    "#98FB98",
+    "#87CEEB",
+    "#FFB6C1",
+    "#FFDAB9",
+    "#FFA500",
+    "#FF69B4",
+    "#B0E0E6",
+    "#98FB98",
+    "#FF6347",
+    "#20B2AA",
+    "#9370DB",
+    "#FF4500",
+    "#F0E68C",
+    "#F08080",
+    "#FFA07A",
+  ];
+  const charSum = initials?.charCodeAt(0) + initials?.charCodeAt(1);
+  const colorIndex = charSum % colors.length;
+  return colors[colorIndex];
 };
