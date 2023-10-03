9e3b9f56336c8e28b5583d8e461bca5aaf8a1159
manasye <manase32@gmail.com>
2023-10-03 11:29:35 +0700

    change test

--- src/utils/test/phoneNumber.test.js
+++ src/utils/test/phoneNumber.test.js
@@ -1,81 +1,77 @@
-import { comparePhoneNumbers } from '../phoneNumber';
+import { comparePhoneNumbers } from "../phoneNumber";
 
-describe('comparePhoneNumbers function', () => {
-    it('should correctly identify new, deleted, and edited phone numbers', () => {
-        const oldPhoneNumbers = [
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' },
-            { id: 3, value: '555-123-4567' }
-        ];
+describe("comparePhoneNumbers function", () => {
+  it("should correctly identify new, deleted, and edited phone numbers", () => {
+    const oldPhoneNumbers = [
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+      { id: 3, value: "08317283045" },
+    ];
 
-        const newPhoneNumbers = [
-            { id: 2, value: '987-654-3210' }, // Unchanged
-            { id: 4, value: '888-888-8888' }, // New
-            { id: 1, value: '999-999-9999' } // Edited
-        ];
+    const newPhoneNumbers = [
+      { id: 2, value: "0867564486065" }, // Unchanged
+      { id: 4, value: "08328985104" }, // New
+      { id: 1, value: "08878814830" }, // Edited
+    ];
 
-        const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
+    const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
 
-        expect(result.newNumbers).toEqual([{ id: 4, value: '888-888-8888' }]);
-        expect(result.deletedNumbers).toEqual([
-            { id: 3, value: '555-123-4567' }
-        ]);
-        expect(result.editedNumbers).toEqual([
-            { id: 1, value: '999-999-9999' }
-        ]);
-    });
+    expect(result.newNumbers).toEqual([{ id: 4, value: "08328985104" }]);
+    expect(result.deletedNumbers).toEqual([{ id: 3, value: "08317283045" }]);
+    expect(result.editedNumbers).toEqual([{ id: 1, value: "08878814830" }]);
+  });
 
-    it('should handle cases where there are no changes', () => {
-        const oldPhoneNumbers = [
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' }
-        ];
+  it("should handle cases where there are no changes", () => {
+    const oldPhoneNumbers = [
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+    ];
 
-        const newPhoneNumbers = [
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' }
-        ];
+    const newPhoneNumbers = [
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+    ];
 
-        const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
+    const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
 
-        expect(result.newNumbers).toEqual([]);
-        expect(result.deletedNumbers).toEqual([]);
-        expect(result.editedNumbers).toEqual([]);
-    });
+    expect(result.newNumbers).toEqual([]);
+    expect(result.deletedNumbers).toEqual([]);
+    expect(result.editedNumbers).toEqual([]);
+  });
 
-    it('should handle cases where all phone numbers are new', () => {
-        const oldPhoneNumbers = [];
+  it("should handle cases where all phone numbers are new", () => {
+    const oldPhoneNumbers = [];
 
-        const newPhoneNumbers = [
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' }
-        ];
+    const newPhoneNumbers = [
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+    ];
 
-        const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
+    const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
 
-        expect(result.newNumbers).toEqual([
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' }
-        ]);
-        expect(result.deletedNumbers).toEqual([]);
-        expect(result.editedNumbers).toEqual([]);
-    });
+    expect(result.newNumbers).toEqual([
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+    ]);
+    expect(result.deletedNumbers).toEqual([]);
+    expect(result.editedNumbers).toEqual([]);
+  });
 
-    it('should handle cases where all phone numbers are deleted', () => {
-        const oldPhoneNumbers = [
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' }
-        ];
+  it("should handle cases where all phone numbers are deleted", () => {
+    const oldPhoneNumbers = [
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+    ];
 
-        const newPhoneNumbers = [];
+    const newPhoneNumbers = [];
 
-        const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
+    const result = comparePhoneNumbers(oldPhoneNumbers, newPhoneNumbers);
 
-        expect(result.newNumbers).toEqual([]);
-        expect(result.deletedNumbers).toEqual([
-            { id: 1, value: '123-456-7890' },
-            { id: 2, value: '987-654-3210' }
-        ]);
-        expect(result.editedNumbers).toEqual([]);
-    });
+    expect(result.newNumbers).toEqual([]);
+    expect(result.deletedNumbers).toEqual([
+      { id: 1, value: "08267448335" },
+      { id: 2, value: "0867564486065" },
+    ]);
+    expect(result.editedNumbers).toEqual([]);
+  });
 });
